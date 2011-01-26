9309045e8ea206a6e9277d59e980b6c28d527ac0
Dennis Stevense <dennis@naquah.net>
2011-01-26 12:16:09 +0100

    Basic e-mail formatting infrastructure.

---
+++ emailkit_message.tpl.php
@@ -0,0 +1,10 @@
+<?php 
+// Developed by Dennis Stevense for Digital Deployment
+?><html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <body>
+    <?php print $message['#children'] ?>
+  </body>
+</html>
