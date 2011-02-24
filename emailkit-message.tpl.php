5fbbe7d005c202b93992f66c351c69c28677a61e
Dennis Stevense <dennis@naquah.net>
2011-02-24 17:34:22 +0100

    Improved documentation in emailkit-message.tpl.php.

--- emailkit-message.tpl.php
+++ emailkit-message.tpl.php
@@ -3,6 +3,8 @@
 
 /**
  * @file This template contains the default markup of a message sent by emailkit.
+ *
+ * @param $message The structured message array. The message (or pieces of it) can be rendered using drupal_render(). (This is inspired by Drupal 7, where it would be rendered using the render() and hide() functions.)
  */
 
 ?><html>
@@ -10,6 +12,18 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   </head>
   <body>
-    <?php print drupal_render($message) ?>
+    <div id="header">
+      <?php print drupal_render($message['header']) ?>
+    </div>
+    <div id="content">
+      <?php print drupal_render($message['content']) ?>
+    </div>
+    <div id="footer">
+      <?php print drupal_render($message['footer']) ?>
+    </div>
+    <?php
+      // This will render out any remaining elements that have not yet been rendered by the above calls
+      print drupal_render($message);
+    ?>
   </body>
 </html>
